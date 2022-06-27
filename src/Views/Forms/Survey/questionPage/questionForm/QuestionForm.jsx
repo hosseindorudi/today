5747fffe71ff95fac827e71178ceb34f5cb448b3
dorodi <dorodi@ctel.net>
2022-06-27 10:59:40 +0430

    question List

--- src/Views/Forms/Survey/questionPage/questionForm/QuestionForm.jsx
+++ src/Views/Forms/Survey/questionPage/questionForm/QuestionForm.jsx
@@ -2,16 +2,116 @@ import { Button, Form } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import './questionForm.css'
 import { QuestionTypeEnum } from '../../../../../data/QuestionTypeEnum' 
-import { useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { CustomReactMultiSelect } from '../../../../../Components/Select/customReactSelect'
+import { enums } from '../../../../../data/Enums'
+import { TabContext } from '../../../../../contexts/TabContextProvider'
+import QuestionList from '../questionList/QuestionList'
+import { questionCreate } from '../../../../../services/questionService'
+import { toast } from "react-toastify";
+import useAxios from '../../../../../customHooks/useAxios'
+import useRequest from '../../../../../customHooks/useRequest'
+
 const QuestionForm = () => {
     const {t} = useTranslation()
     const [title,setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [color, setColor] = useState("#000000")
-    const [qType, setQType] = useState()
+    const [enumQuestion, setEnumQuestion] = useState([])
+    const [questionSelect, setQuestionSelect] = useState("")
+    const colors = ['#470063', '#B30089', '#F62DAE', '#FD96A9', 
+    '#B846E6',
+    '#4560E6',
+    '#7784C4',
+    '#0BD9E4',
+    '#7E0BE3',
+    '#0BE34C',
+    '#A2E30B',
+    '#E3C60B',
+    '#E37B0B',
+    '#E3410B',
+    ];
+    const abortController = new AbortController();
+
+    const tabContext = useContext(TabContext);
+    const [response, loading, fetchData, setResponse] = useAxios();
+    const request=useRequest();
+    const handleClickMenu = () => {
+        tabContext.addRemoveTabs(
+        {
+            title: "routes.questionForm",
+            path: "/Survey/Question/Create",
+            Component: QuestionForm,
+            access: enums.Survey_QuestionPage_Create_w,
+        },
+        "remove"
+        );
+        tabContext.addRemoveTabs(
+        {
+            title: "routes.questionList",
+            path: "/Survey/Question/Read",
+            Component: QuestionList,
+            access: enums.Survey_QuestionPage_Read_r,
+        },
+
+        "add"
+        );
+    };
+
+    const handleError=(message)=>{
+  
+        toast.error(message, {
+          position: toast.POSITION.BOTTOM_CENTER,
+        });
+        
+      }
+      const handleSeccess=(message)=>{
+        
+        toast.success(message, {
+          position: toast.POSITION.BOTTOM_CENTER,
+        });
+      }
+
+      useEffect(() => {
+        if(response){
+          response.Result?handleSeccess(response.message):handleError(response.message)
+          response.Result && handleClickMenu() ;
+        }
+        setResponse(undefined)
+           // eslint-disable-next-line react-hooks/exhaustive-deps 
+      }, [response])
+
+    useEffect(()=> {
+        Object.keys(QuestionTypeEnum).map((key, index) => {
+
+            setEnumQuestion((prev) => [...prev, {value: QuestionTypeEnum[key], label: t(key), color: colors[Math.floor(Math.random() * colors.length)]}])
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps 
+    }, [])
 
     const handleSubmit =(e) => {
+
         e.preventDefault()
+        if (title !=="" & description!==""){
+        fetchData({
+            method: "POST",
+            url: questionCreate,
+            headers: {
+              accept: "*/*",
+            },
+            signal:abortController.signal,
+            data: {
+              
+              Id:0,
+              QuestionnaireType_Id: Number(questionSelect),
+              Title: title,
+              Description: description,
+              Request:request,
+            },
+          });
+        }else {
+            handleError("لطفا فیلد ها را وارد کنید")
+        }
     }
 
 
@@ -36,15 +136,17 @@ const QuestionForm = () => {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>{t("questionType")}</Form.Label>
-                    <Form.Select onChange={(e)=> setQType(e.target.value)}>
-                        {
-                        Object.keys(QuestionTypeEnum).map((key, index) => {
-                            return(
-                                <option value={QuestionTypeEnum[key]} key={index}>{t(key)}</option>
-                            )
-                        })
-                        }
-                    </Form.Select>
+                      
+                    <CustomReactMultiSelect
+                      isMulti={false}
+                      options={enumQuestion}
+                      value={questionSelect}
+                      onchangeHandler={(e) => setQuestionSelect(e.value)}
+                      placeholder={t("questionType")}
+                    />
+
+                  
+                   
                 </Form.Group>
                 <Form.Group className="mb-3 questionColorPicker" controlId="exampleForm.ControlTextarea1">
                     
