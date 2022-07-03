a5dc141f5012b0885488ea90ce7dfbb2d4469f96
hossein <you@example.com>
2022-07-03 14:50:28 +0430

    hossein

--- src/Components/Table/LogModal/LogModal.jsx
+++ src/Components/Table/LogModal/LogModal.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
 import "./logModal.css";
 import { t } from "i18next";
+import AppContext from "../../../contexts/AppContext";
+import { convertUTC } from "../../../validation/functions";
 const LogModal = (props) => {
   const logs = props.logs;
     const [showDescription, setShowDescription] = useState(false);
@@ -10,6 +12,7 @@ const LogModal = (props) => {
     setDescription(data)
     setShowDescription(true)
   }
+  const {app} = useContext(AppContext)
   return (
     <>
     {showDescription &&(
@@ -17,14 +20,50 @@ const LogModal = (props) => {
    
     <Modal
     onHide={()=>setShowDescription(false)}
-    size="sm"
+    size="md"
+   
     aria-labelledby="contained-modal-title-vcenter"
     centered
     show={showDescription}
   >
     <Modal.Header closeButton></Modal.Header>
-    <Modal.Body>
-      <p>{description}</p>
+    <Modal.Body  className="logSecondModal">
+      {console.log(description.split('\n'))}
+
+      <p>{description.split('\n').map((desc, i) => {
+
+              if ( i === description.split('\n').length -2 ) {
+                console.log(desc.split(' '))
+                let date = desc.split(' ');
+                // let newdate = date[1].split("T")
+                let a = date[1].split("T")
+                a[0] = convertUTC(a[0])
+                date[1] = a.join("   ")
+                a = date[3].split("T")
+                a[0] = convertUTC(a[0])
+                date[3] = a.join("   ")
+                console.log(date.join(" "))
+                return <>{date.join(" ")} <br /></>
+              }else {
+                return <>{desc} <br /></>
+              }
+              
+               
+
+        // if(desc === '\n' ){
+        //   return <><br /></>
+        // }
+        // else if (desc === '>'){
+        //   return <b style={{color: "blue"}}>{desc}</b>
+        // }
+        // else if (desc === ':'){
+        //   return <b style={{color: "green"}}>{desc}</b>
+        // }
+        
+        // else {
+        //  return  desc
+        // }
+      } )}</p>
     </Modal.Body>
   </Modal>
    )}
@@ -37,11 +76,11 @@ const LogModal = (props) => {
     >
       <Modal.Header closeButton></Modal.Header>
       <Modal.Body>
-        <Table >
+        <Table size="sm" className="logModalTable">
           <thead>
             <tr>
               {Object.keys(logs[0]).map((k) => (
-                <th key={k}>{k}</th>
+                <th key={k}>{t(k)}</th>
               ))}
             </tr>
           </thead>
@@ -49,7 +88,10 @@ const LogModal = (props) => {
             {logs.map((log, index) => (
               <tr key={index}>
                 {Object.keys(log).map((key, index) => (
-                  <td key={key+index}>{key==="Description"&&log[key].length>0?<Button variant="link" onClick={()=>handleClickMore(log[key])} >{t("logview")}</Button>:log[key]}</td>
+                  <>
+                  {console.log(key)}
+                  <td key={key+index}>{key==="Description"&&log[key].length>0?(<Button variant="link" className="logoLinkVar" onClick={()=>handleClickMore(log[key])} >{t("logview")}</Button>): key === "DateSet" ?  convertUTC(log[key])   : log[key]}</td>
+                  </>
                 ))}
               </tr>
             ))}
