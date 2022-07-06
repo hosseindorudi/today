c24f6bdbed393d02e324e00f1e1a535f993ee2b8
hossein <hdorudi@gmail.com>
2022-07-06 21:31:50 +0430

    dashboard and seccess login completed

--- src/Views/Forms/panel/loginlogs/SuccessLogin.jsx
+++ src/Views/Forms/panel/loginlogs/SuccessLogin.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import "../../../../assets/css/table.css";
+import React, { useRef, useState } from "react";
+
+import { enums } from "../../../../data/Enums";
+import CustomTable from "../../../../Components/Table/Table/CustomTable";
+import {
+  LoginHistoryRead,
+  LoginHistoryReadPaging,
+  LoginHistoryFavorite,
+  LoginHistoryExportId,
+  LoginHistoryExport,
+  LoginHistoryLog,
+  LoginHistorySetUnselectedColumn,
+  LoginHistoryAccessList,
+} from "../../../../services/loginHistoryServices";
+import useWindowSize from "../../../../customHooks/useWindowSize";
 
 const SuccessLogin = () => {
-  return (
-    <div>SuccessLogin</div>
-  )
-}
+  const childRef = useRef();
+  const filteredColumns = [
+    "IsLimited",
+    "Id",
+    "Registrar",
+    "Group_Id",
+    "Language_EId",
+    "SourceType",
+    "Operator_Id",
+  ];
+  const [mobileModal, setMobileModal] = useState(false);
+  const [mobileModalButtons, setMobileModalButtons] = useState(false);
+  const [mobileModalColumns, setMobileModalColumns] = useState(false);
+  const widthOFScreen = useWindowSize().width;
 
-export default SuccessLogin
+  const handleClickHelp = () => {
+    window.open("https://www.google.com");
+  };
+
+  return (
+    <>
+      <CustomTable
+        ref={childRef}
+        ReadApi={LoginHistoryRead}
+        deleteApi={""}
+        unSelectedAPI={LoginHistorySetUnselectedColumn}
+        sampleUrl={""}
+        fileCheckURL={""}
+        importURL={""}
+        logApi={LoginHistoryLog}
+        exportId={LoginHistoryExportId}
+        changePasswordURL={""}
+        addObject={""}
+        exportAccess={enums.Operator_LoginHistory_Export_r}
+        exportLink={LoginHistoryExport}
+        importAccess={""}
+        logAccess={""}
+        // enums.Operator_LoginHistory_Log_r
+        readPagingApi={LoginHistoryReadPaging}
+        accessListAccess={enums.Operator_LoginHistory_Read_r}
+        accessListApi={LoginHistoryAccessList}
+        favouriteApi={LoginHistoryFavorite}
+        handleClickHelp={handleClickHelp}
+        addFormAccess={""}
+        filteredColumns={filteredColumns}
+        deleteAccess={""}
+        editAccess={""}
+        permissionsAccess={""}
+        changePasswordAccess={""}
+        getOneRecord={""}
+        setUpdate={""}
+        mobileModal={mobileModal}
+        setMobileModal={setMobileModal}
+        widthOFScreen={widthOFScreen}
+        mobileModalButtons={mobileModalButtons}
+        setMobileModalButtons={setMobileModalButtons}
+        setMobileModalColumns={setMobileModalColumns}
+        mobileModalColumns={mobileModalColumns}
+      />
+    </>
+  );
+};
+
+export default SuccessLogin;
---
+++ src/services/loginHistoryServices.js
@@ -0,0 +1,9 @@
+export const LoginHistoryRead = "/Operator/LoginHistory/Read";
+export const LoginHistoryReadPaging = "/Operator/LoginHistory/ReadPaging";
+export const LoginHistoryFavorite = "/Operator/LoginHistory/SetToFavorite";
+export const LoginHistoryExportId = "/Operator/LoginHistory/ExportId";
+export const LoginHistoryExport = "/Operator/LoginHistory/Export";
+export const LoginHistoryLog = "/Operator/LoginHistory/Log";
+export const LoginHistorySetUnselectedColumn =
+  "/Operator/LoginHistory/SetUnselectedColumn";
+export const LoginHistoryAccessList = "/Operator/LoginHistory/AccessList";
