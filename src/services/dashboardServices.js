db591c0e898732c0d1ba1e7b615c25d78ab42932
hossein <hdorudi@gmail.com>
2022-07-08 20:10:55 +0430

    dashboard code

--- src/services/dashboardServices.js
+++ src/services/dashboardServices.js
@@ -2,4 +2,4 @@ export const homeDashboard = "/Operator/Dashboard/Read";
 export const createNoteDashboard = "/Operator/Dashboard/CreateNote";
 export const getOneRecordDashboard = "/Operator/Dashboard/GetOneRecord";
 export const updateDashboard = "/Operator/HomDashboarde/UpdateNote";
-export const deleteDashboard = "/Operator/Dashboard/DeleteNote";
+export const deleteNoteDashboard = "/Operator/Dashboard/DeleteNote";
