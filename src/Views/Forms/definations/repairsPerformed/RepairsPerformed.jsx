c967f9362ee1f3721fa6ef4537c721e48e04bc58
hossein <hdorudi@gmail.com>
2022-07-08 22:27:30 +0430

    repaire performed changed

--- src/Views/Forms/definations/repairsPerformed/RepairsPerformed.jsx
+++ src/Views/Forms/definations/repairsPerformed/RepairsPerformed.jsx
@@ -1,20 +1,25 @@
 import "./repairsPerformed.css";
-import useAxios from '../../../../customHooks/useAxios';
-import useRequest from '../../../../customHooks/useRequest'
+import useAxios from "../../../../customHooks/useAxios";
+import useRequest from "../../../../customHooks/useRequest";
 import { Button, Form } from "react-bootstrap";
-import { useEffect, useState } from "react";
-import { 
+import { useEffect, useState, useCallback } from "react";
+import {
   handleError,
   createSelectOptions,
-  defintionInputs, 
+  defintionInputs,
 } from "../../../../validation/functions";
 import FormInput from "../../../../Components/periodity/formInput/FormInput";
 import { modelReadTitle } from "../../../../services/modelService";
-import { repairsPerformedReadTitle } from "../../../../services/repairsPerformed";
-import axios from 'axios'
+import {
+  repairsPerformedReadTitle,
+  repairsPerformedCreate,
+  repairsPerformedRead,
+} from "../../../../services/repairsPerformed";
+import axios from "axios";
 import { useTranslation } from "react-i18next";
 import { CustomReactMultiSelect } from "../../../../Components/Select/customReactSelect";
 import { toast } from "react-toastify";
+
 const RepairsPerformed = () => {
   const [response, loading, fetchData, setResponse] = useAxios();
   const [validated, setValidated] = useState(false);
@@ -23,6 +28,7 @@ const RepairsPerformed = () => {
   const [model, setModel] = useState(undefined);
   const [perfomedGroupOptions, setPerformedGroupOptions] = useState([]);
   const [performedGroup, setPerformedGroup] = useState(undefined);
+  const [perfomedData, setPerformedData] = useState([]);
   const request = useRequest();
   const abortController = new AbortController();
   const [values, setValues] = useState({
@@ -39,7 +45,7 @@ const RepairsPerformed = () => {
     toast.success(t("item.created"), {
       position: toast.POSITION.TOP_CENTER,
     });
-  }
+  };
   const createParams = (service) => {
     const params = {
       method: "POST",
@@ -53,17 +59,33 @@ const RepairsPerformed = () => {
   };
   const getDatas = () => {
     const modelTitles = axios.request(createParams(modelReadTitle));
-    const perfomedGroupTitles = axios.request(createParams(repairsPerformedReadTitle));
+    const perfomedGroupTitles = axios.request(
+      createParams(repairsPerformedReadTitle)
+    );
+    const getAllData = axios.request(createParams(repairsPerformedRead));
     axios
-      .all([modelTitles, perfomedGroupTitles])
+      .all([modelTitles, perfomedGroupTitles, getAllData])
       .then(
         axios.spread((...allData) => {
           allData[0].data?.Result
             ? setModelOptions(createSelectOptions(allData[0].data.Title))
             : handleError(allData[0].data.Message);
           allData[1].data?.Result
-            ? setPerformedGroupOptions(createSelectOptions(allData[1].data.Title))
+            ? setPerformedGroupOptions(
+                createSelectOptions(allData[1].data.Title)
+              )
             : handleError(allData[1].data.Message);
+          allData[2].data?.Result
+            ? setPerformedData(allData[2].data.Title)
+            : handleError(allData[2].data.Message);
+          setModel(undefined);
+          setPerformedGroup(undefined);
+          setValues({
+            title: "",
+            color: "#000000",
+            periority: 1,
+            desc: "",
+          });
         })
       )
       .catch((error) => {
@@ -71,7 +93,7 @@ const RepairsPerformed = () => {
       });
   };
   useEffect(() => {
-    getDatas();
+    handleResponseFunc(response, "READ");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   useEffect(() => {
@@ -85,64 +107,123 @@ const RepairsPerformed = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [response]);
 
+  const handleResponseFunc = useCallback(
+    (response, type) => {
+      switch (type) {
+        // case "DELETE":
+        //   handleDeleted();
+        //   break;
+        case "READ":
+          getDatas();
+          break;
+        case "CREATE":
+          getDatas();
+          break;
+
+        default:
+          break;
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
   const handleSubmit = (e) => {
-      e.preventDefault()
-  }
+    e.preventDefault();
+    fetchData({
+      method: "POST",
+      url: repairsPerformedCreate,
+      headers: {
+        accept: "*/*",
+      },
+      data: {
+        Request: request,
+        Id: 0,
+        Parent_Id: performedGroup !== undefined ? performedGroup.value : 0,
+        Model_Id: model?.value,
+        Title: values.title,
+        Priority: values.periority,
+        Description: values.desc,
+        Color: values.color.substring(1),
+        SourceType: 0,
+        Registrar: 0,
+        DateSet: "2022-06-19T16:43:29.709Z",
+      },
+      signal: abortController.signal,
+    });
+
+    handleResponseFunc(response, "CREATE");
+  };
   return (
-
     <>
-
       <div className="repairedPerformedMain">
-        <div className="repairedPerformedRight">asdasd</div>
-        <div className="repairedPerformedLeft">
-            
-              <Form
-                className="periorityForm"
-                noValidate
-                validated={validated}
-                onSubmit={handleSubmit}
-              >
-                <b>{t("/Definition/RepairsPerformed/Write")}</b>
-                <div className="repairRowPerformed">
-                  <Form.Group className="mb-0.5 repairsPerformedItem" controlId={"model"}>
-                    <Form.Label>{t("Group")}</Form.Label>
-                    <CustomReactMultiSelect
-                      isMulti={false}
-                      options={perfomedGroupOptions}
-                      value={performedGroup}
-                      onchangeHandler={(e) => setPerformedGroup(e)}
-                      placeholder={t("Group")}
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-.5 repairsPerformedItem" controlId={"model"}>
-                    <Form.Label>{t("model")}</Form.Label>
-                    <CustomReactMultiSelect
-                      isMulti={false}
-                      options={modelOptions}
-                      value={model}
-                      onchangeHandler={(e) => setModel(e)}
-                      placeholder={t("model")}
-                    />
-                  </Form.Group>
-                 
-                </div>
-                {defintionInputs(
-                  values,
-                  t("/Definition/RepairsPerformed/Read"),
-                  t("RepairsPerformed_errorMSG")
-                ).map((input) => (
-                  <FormInput performedGroup={model}  isRepair={true} key={input.id} {...input} onChange={onChangeHandler} />
-                ))}
-                <Button  disabled={loading} type="submit">
-                  {t("submit")}
-                </Button>
-              </Form>
- 
+        <div className="repairePerformUp">
+          <div className="repairedPerformedRight">asdasd</div>
+          <div className="repairedPerformedLeft">
+            <Form
+              className="periorityForm"
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+            >
+              <b>{t("/Definition/RepairsPerformed/Write")}</b>
+              <div className="repairRowPerformed">
+                <Form.Group
+                  className="mb-0.5 repairsPerformedItem"
+                  controlId={"model"}
+                >
+                  <Form.Label>{t("Group")}</Form.Label>
+                  <CustomReactMultiSelect
+                    isMulti={false}
+                    options={perfomedGroupOptions}
+                    value={performedGroup}
+                    onchangeHandler={(e) => setPerformedGroup(e)}
+                    placeholder={t("Group")}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-.5 repairsPerformedItem"
+                  controlId={"model"}
+                >
+                  <Form.Label>{t("model")}</Form.Label>
+                  <CustomReactMultiSelect
+                    isMulti={false}
+                    options={modelOptions}
+                    value={model}
+                    onchangeHandler={(e) => setModel(e)}
+                    placeholder={t("model")}
+                  />
+                </Form.Group>
+              </div>
+              {defintionInputs(
+                values,
+                t("/Definition/RepairsPerformed/Read"),
+                t("RepairsPerformed_errorMSG")
+              ).map((input) => (
+                <FormInput
+                  performedGroup={model}
+                  isRepair={true}
+                  key={input.id}
+                  {...input}
+                  onChange={onChangeHandler}
+                />
+              ))}
+              <Button disabled={loading} type="submit">
+                {t("submit")}
+              </Button>
+            </Form>
+          </div>
+        </div>
+        <div className="repairPerformDown">
+          <button>first</button>
+          <button>first</button>
+          <button>first</button>
+          <button>first</button>
+          <button>first</button>
+          <button>first</button>
         </div>
       </div>
-    
     </>
-
   );
 };
 
