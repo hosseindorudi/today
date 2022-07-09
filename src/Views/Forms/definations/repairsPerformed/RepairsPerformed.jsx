55580e80a06837b1661632c192c9ccc05aab597d
hossein <you@example.com>
2022-07-09 14:39:59 +0430

    repairedPerformed

--- src/Views/Forms/definations/repairsPerformed/RepairsPerformed.jsx
+++ src/Views/Forms/definations/repairsPerformed/RepairsPerformed.jsx
@@ -2,25 +2,49 @@ import "./repairsPerformed.css";
 import useAxios from "../../../../customHooks/useAxios";
 import useRequest from "../../../../customHooks/useRequest";
 import { Button, Form } from "react-bootstrap";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useContext } from "react";
+import Swal from "sweetalert2";
+
 import {
   handleError,
   createSelectOptions,
   defintionInputs,
 } from "../../../../validation/functions";
+import * as fa from 'react-icons/fa'
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
 import FormInput from "../../../../Components/periodity/formInput/FormInput";
 import { modelReadTitle } from "../../../../services/modelService";
 import {
   repairsPerformedReadTitle,
   repairsPerformedCreate,
   repairsPerformedRead,
+  repairsPerformedLog,
+  repairsPerformedSampleFile,
+  repairsPerformedCheckFile,
+  repairsPerformedImport,
+  repairsPerformedExport,
+  repairsPerformedSetToFavorite,
+  repairsPerformedDelete,
 } from "../../../../services/repairsPerformed";
 import axios from "axios";
 import { useTranslation } from "react-i18next";
 import { CustomReactMultiSelect } from "../../../../Components/Select/customReactSelect";
 import { toast } from "react-toastify";
-
+import { data } from "autoprefixer";
+import da from "date-fns/esm/locale/da/index.js";
+import useButtonAccess from "../../../../customHooks/useButtonAccess";
+import { enums } from "../../../../data/Enums";
+import BackDrop from "../../../../Components/backDrop/BackDrop";
+import LogModal from "../../../../Components/Table/LogModal/LogModal";
+import ImportCSV from "../../../../Components/Table/ImportCSVButton/ImportCSV";
+import ExportAllButton from "../../../../Components/Table/ExportButton/ExportAllButton";
+import AppContext from "../../../../contexts/AppContext";
+import useGeoLocation from "../../../../customHooks/useGeoLocation";
 const RepairsPerformed = () => {
+  const { app } = useContext(AppContext);
   const [response, loading, fetchData, setResponse] = useAxios();
   const [validated, setValidated] = useState(false);
   const [type, setType] = useState("");
@@ -31,6 +55,12 @@ const RepairsPerformed = () => {
   const [perfomedData, setPerformedData] = useState([]);
   const request = useRequest();
   const abortController = new AbortController();
+  const [haveAccess] = useButtonAccess();
+  const [showLogModal, setShowLogModal] = useState(false)
+  const [log, setLog] = useState(null);
+  const [IsFavorite, setIsFavorite] = useState(false);
+  const accessToken = localStorage.getItem("token");
+  const location = useGeoLocation();
   const [values, setValues] = useState({
     title: "",
     color: "#000000",
@@ -76,8 +106,9 @@ const RepairsPerformed = () => {
               )
             : handleError(allData[1].data.Message);
           allData[2].data?.Result
-            ? setPerformedData(allData[2].data.Title)
+            ? setPerformedData(allData[2].data.Record)
             : handleError(allData[2].data.Message);
+          setIsFavorite(allData[2].data.IsFavorite)
           setModel(undefined);
           setPerformedGroup(undefined);
           setValues({
@@ -92,6 +123,31 @@ const RepairsPerformed = () => {
         handleError(error.message);
       });
   };
+
+
+  // const handleViewer = () =>{
+  //   perfomedVar.map((data) => {
+
+  //   })
+  // }
+
+
+  // useEffect(()=> {
+  //   let perfomedVar = perfomedData;
+  //   let finalData = []
+    
+  //   perfomedVar.map((data) => {
+  //     if(data.Parent_Title === " ") {
+  //       finalData.push({id:data.Id,name:data.Title, children:[]})
+  //       perfomedVar.map((d) => {
+  //         if(d.Parent_Title === data.Title) {
+              
+  //         }
+  //       })
+  //     }
+  //   })
+  // },[perfomedData])
+
   useEffect(() => {
     handleResponseFunc(response, "READ");
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -99,7 +155,7 @@ const RepairsPerformed = () => {
   useEffect(() => {
     if (response) {
       response.Result
-        ? handleResponse(response)
+        ? handleResponseFunc(response, type)
         : handleError(response.Message);
       setResponse(undefined);
     }
@@ -110,15 +166,21 @@ const RepairsPerformed = () => {
   const handleResponseFunc = useCallback(
     (response, type) => {
       switch (type) {
-        // case "DELETE":
-        //   handleDeleted();
-        //   break;
+        case "DELETE":
+          handleDeleted();
+          break;
         case "READ":
           getDatas();
           break;
         case "CREATE":
           getDatas();
           break;
+          case "LOG":
+            logResponse(response);
+            break;
+          case "FAVORITE":
+            favorited();
+            break;
 
         default:
           break;
@@ -127,6 +189,28 @@ const RepairsPerformed = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
+
+  const logResponse = (res) => {
+    if (res.Log.length ===0)  {
+      return toast.info(t("noDataFound.table"), {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
+    setLog(res.Log);
+    setShowLogModal(true);
+  };
+  const handleClickLog = () => {
+    setType("LOG");
+    fetchData({
+      method: "POST",
+      url: repairsPerformedLog,
+      headers: {
+        accept: "*/*",
+      },
+      data: request,
+      signal: abortController.signal,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -154,12 +238,148 @@ const RepairsPerformed = () => {
 
     handleResponseFunc(response, "CREATE");
   };
+
+  const importSuccess = (message) => {
+    toast.success(message, {
+      position: toast.POSITION.TOP_CENTER,
+    });}
+
+    const favorited = () => {
+      setIsFavorite(true);
+      toast.success(t("favorited"), {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    };
+
+    const handleClickFav = () => {
+      setType("FAVORITE");
+      fetchData({
+        method: "POST",
+        url: repairsPerformedSetToFavorite,
+        headers: {
+          accept: "*/*",
+        },
+        data: request,
+        signal: abortController.signal,
+      });
+    };
+
+    const handleClickHelp = () => {
+      window.open("https://www.google.com");
+    };
+
+    const handleDeleted = () => {
+      Swal.fire(
+        t("sweetAlert.deleted"),
+        t("sweetAlert.recordDeleted"),
+        "success"
+      );
+  
+      getDatas();
+    };
+
+    const EditeOneRecord =(data) => {
+      console.log(data)
+     setModel(modelOptions.filter(f => f.label === data.Model_Title))
+     data.Parent_Id === 0 ? setPerformedGroup(undefined) :
+     setPerformedGroup(perfomedGroupOptions.filter(f => f.label === data.Parent_Title))
+
+      setValues({
+        title: data.Title,
+        color: `#${data.Color}`,
+        periority: data.Priority,
+        desc: data.Description,
+      });
+    }
+
+    const deleteOneRecord = (id) => {
+      Swal.fire({
+        title: t("table.deleteTitle"),
+        text: t("table.noReturn"),
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: t("sweetAlert.yes"),
+        cancelButtonText: t("sweetAlert.cancel"),
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setType("DELETE");
+          fetchData({
+            method: "POST",
+            url: repairsPerformedDelete,
+            headers: {
+              accept: "*/*",
+            },
+  
+            data: {
+              Request: {
+                Language: app.langCode,
+                Token: accessToken ? accessToken : "",
+                Latitude: location.loaded ? location.coordinates.lat : 0,
+                Longitude: location.loaded ? location.coordinates.lng : 0,
+              },
+              Id: id,
+            },
+  
+            signal: abortController.signal,
+          });
+        }
+      });
+    };
+
+  const handleTreeView = (data) => {
+    
+    
+      return (<div className="treeViewRow">
+        <TreeItem nodeId={String(data.Id)} label={data.Title} style={{width:"100%"}} >
+                  {perfomedData.map(d => (
+                    d.Parent_Id === data.Id ? handleTreeView(d)
+                      
+                     : null
+                  )) }
+                  
+              </TreeItem>
+              <button className="rowViewBtn" onClick={() => deleteOneRecord(data.Id)}><fa.FaTrash/></button>
+              <button className="rowViewBtnEdit" onClick={() => EditeOneRecord(data)}><fa.FaRegEdit/></button>
+              {data.Parent_Id === 0 && <button className="rowViewBtnFee"><fa.FaDollarSign/></button>}
+      </div>)
+  
+  }
+
   return (
     <>
+    {loading && <BackDrop open={true} />}
+      {showLogModal && (
+        <LogModal
+          onHide={() => setShowLogModal(false)}
+          logs={log}
+          show={showLogModal}
+        />
+      )}
       <div className="repairedPerformedMain">
         <div className="repairePerformUp">
-          <div className="repairedPerformedRight">asdasd</div>
+          <div className="repairedPerformedRight">
+          <TreeView
+            aria-label="file system navigator"
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpandIcon={<ChevronRightIcon />}
+            style={{direction:"ltr"}}
+            sx={{ height: "100%", flexGrow: 1, width:"100%",  overflowY: 'auto' }}
+          >
+            
+            {perfomedData.map((data) => (
+              data.Parent_Id === 0 &&
+
+               handleTreeView(data)
+              
+            ))}
+          </TreeView> 
+
+
+          </div>
           <div className="repairedPerformedLeft">
+            {console.log(perfomedData)}
             <Form
               className="periorityForm"
               noValidate
@@ -215,12 +435,46 @@ const RepairsPerformed = () => {
           </div>
         </div>
         <div className="repairPerformDown">
-          <button>first</button>
-          <button>first</button>
-          <button>first</button>
-          <button>first</button>
-          <button>first</button>
-          <button>first</button>
+        {haveAccess(enums.Definition_RepairsPerformed_Log_r) && (
+            <button
+              className="reactTableParentLogButton"
+              title="log"
+              onClick={handleClickLog}
+            >
+              <fa.FaHistory />
+            </button>
+          )}
+          {haveAccess(enums.Definition_RepairsPerformed_Import_w) && (
+            <ImportCSV
+              importSuccess={importSuccess}
+              sampleUrl={repairsPerformedSampleFile}
+              fileCheckURL={repairsPerformedCheckFile}
+              importURL={repairsPerformedImport}
+            />
+          )}
+          {haveAccess(enums.Definition_RepairsPerformed_Export_r) && (
+            <ExportAllButton
+              repaireExp = {true}
+              exportLink={repairsPerformedExport}
+            />
+          )}
+          <button
+            disabled={IsFavorite}
+            title="favorite"
+            className={`reactTableParentFavoritButton ${
+              IsFavorite ? "favactive" : ""
+            }`}
+            onClick={handleClickFav}
+          >
+            <fa.FaRegStar />
+          </button>
+          <button
+            className="reactTableParentHelpButton"
+            onClick={handleClickHelp}
+            title="help"
+          >
+            <fa.FaQuestionCircle />
+          </button>
         </div>
       </div>
     </>
