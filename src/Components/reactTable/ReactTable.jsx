e853b88e5482e0ab473b5128f50c90aa8e77d086
mehrzad7394 <mehrzad.ardeshiri@gmail.com>
2022-08-01 12:15:30 +0430

    new changes on import

--- src/Components/reactTable/ReactTable.jsx
+++ src/Components/reactTable/ReactTable.jsx
@@ -1,19 +1,59 @@
 import React from 'react'
-import { Table } from 'react-bootstrap';
+import { Form, Table } from 'react-bootstrap';
 import { useTable } from "react-table";
+import { CustomReactMultiSelect } from '../Select/customReactSelect';
 const styles = {
     table: {
       fontSize: 11,
       whiteSpace: "nowrap",
       textAlign: "center",
     },
+    td:{
+      display:"flex",
+     
+    }
   };
+
+
+  const EditableCell = ({
+    value: initialValue,
+    row: { index },
+    column: { id },
+    updateMyData, // This is a custom function that we supplied to our table instance
+    isEditable
+  }) => {
+    // We need to keep and update the state of the cell normally
+    const [value, setValue] = React.useState(initialValue)
+  
+    const onChange = e => {
+      setValue(e.target.value)
+    }
+  
+    // We'll only update the external data when the input is blurred
+    const onBlur = () => {
+      updateMyData(index, id, value)
+    }
+  
+    // If the initialValue is changed external, sync it up with our state
+    React.useEffect(() => {
+      setValue(initialValue)
+    }, [initialValue])
+  
+    return isEditable?<div style={styles.td}><input value={value} onChange={onChange} onBlur={onBlur} /><select><option>{id}</option></select></div>:value
+  }
+
   const defaultPropGetter = () => ({})
-const ReactTable = ({columns,data, getCellProps = defaultPropGetter,}) => {
+  const defaultColumn = {
+    Cell:EditableCell,
+  }
+const ReactTable = ({columns,data, getCellProps = defaultPropGetter,updateMyData,isEditable}) => {
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns,
       data,
+      defaultColumn,
+      updateMyData,
+      isEditable
     });
   return (
     <Table style={styles.table} responsive {...getTableProps()}>
