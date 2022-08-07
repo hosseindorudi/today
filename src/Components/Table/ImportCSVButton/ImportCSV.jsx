c5dd26ad17d69e8bdfbed7857068b085c8a7885b
Mehrzad <mehrzad.ardeshiri@gmail.com>
2022-08-07 14:36:46 +0430

    ui modal to tab

--- src/Components/Table/ImportCSVButton/ImportCSV.jsx
+++ src/Components/Table/ImportCSVButton/ImportCSV.jsx
@@ -3,26 +3,25 @@ import * as fa from 'react-icons/fa'
 import { TabContext } from '../../../contexts/TabContextProvider'
 import ImportCSVModal from './ImportCSVModal'
 import ImportUI from './importUIComponent/ImportUI'
-import ImportUIModal from './ImportUIModal'
 const ImportCSV = (props) => {
   const [modalShow, setModalShow] = useState(false)
   const tabContext = useContext(TabContext);
-  const [modalImportUI, setModalImportUI] = useState(false)
   const [file, setFile] = useState(null)
   const [withHeader, setWithHeader] = useState(true);
   const handleOpenModalCSV=()=>{
     setFile(null)
     setWithHeader(true)
     setModalShow(true)
-  } 
-  const handleUIClick=()=>{
-    setModalShow(false)
     tabContext.addRemoveTabs(
       {
-        path: "importUI",
+        path: "ImportUI",
       },
       "remove"
     );
+  } 
+  const handleUIClick=()=>{
+    setModalShow(false)
+    
     tabContext.addRemoveTabs(
       {
         Component: ImportUI,
@@ -35,11 +34,6 @@ const ImportCSV = (props) => {
       },
       "add"
     );
-    // setModalImportUI(true)
-  }
-  const importSuccess=(message)=>{
-    setModalImportUI(false)
-    props.importSuccess(message)
   }
   return (
     <>
@@ -47,10 +41,7 @@ const ImportCSV = (props) => {
     <ImportCSVModal onHide={()=>setModalShow(false)} show={modalShow} importSuccess={props.importSuccess} sampleUrl={props.sampleUrl} fileCheckURL={props.fileCheckURL} importURL={props.importURL} handleUIClick={handleUIClick}
     file={file} setFile={setFile} withHeader={withHeader} setwithheader={setWithHeader}
     />
-    } 
-    {modalImportUI &&(
-      <ImportUIModal onHide={()=>setModalImportUI(false)} show={modalImportUI} columnInfo={props.columnInfo} file={file} withHeader={withHeader} importarray={props.importarray} importSuccess={importSuccess}/>
-    )}
+    }
     <button  className="reactTableParentImportButton" title="importCSV" onClick={handleOpenModalCSV}>
     <fa.FaFileCsv />
   </button>
