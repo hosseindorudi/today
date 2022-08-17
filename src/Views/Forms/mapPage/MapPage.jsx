81b14d038cd01b56c3556dbaf9af0bcdd92d844c
hossein <you@example.com>
2022-08-17 15:15:49 +0430

    map

--- src/Views/Forms/mapPage/MapPage.jsx
+++ src/Views/Forms/mapPage/MapPage.jsx
@@ -14,7 +14,7 @@ import useAxios from "../../../customHooks/useAxios";
 import useRequest from "../../../customHooks/useRequest";
 import { handleSeccess } from "../../../validation/functions";
 import { LoginHistoryReportDispersion } from "../../../services/loginHistoryServices";
-import {Chart} from 'chart.js'
+
 const MapPage = () => {
   const { t } = useTranslation();
   const [isActive, setIsActive] = useState(true);
@@ -27,44 +27,7 @@ const MapPage = () => {
   const request = useRequest();
   const [type, setType] = useState("");
   const [EId, setEId] = useState(1)
-  const ctx = document.getElementById('myChart');
-
-
-  const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
+ 
 
   const handleResponse = (response, type) => {
     switch (type) {
@@ -186,7 +149,6 @@ const MapPage = () => {
         <Button className="btnShowMap" onClick={handleSubmit} disabled={loading}>{t("show")}</Button>
       </div>
       <div className="reportChart">
-      <canvas id="myChart" width="400" height="400"></canvas>
       </div>
     </div>
   );
